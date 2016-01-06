4dc3bb2f2e4e97486dcc494866082201487f30f1
Hialan Liu <hialan.liu@gmail.com>
2016-01-06 18:14:32 +0800

    add code

---
+++ src/window_manager_client.js
@@ -0,0 +1,38 @@
+'use strict';
+var ipc = require('electron').ipcRenderer;
+
+function WindowManagerClient(window_name, dispatcher) {
+  this.window_name = window_name;
+  this.dispatcher = dispatcher;
+}
+
+function sendToServer(action, data) {
+  var command = {
+    action: action,
+    data: data
+  };
+  ipc.send('WindowManager', command);
+}
+
+WindowManagerClient.prototype.init = function() {
+  ipc.on('WindowManager', function(event, arg) {
+    this.dispatcher(event, arg);
+  }.bind(this));
+
+  // notify main process init done
+  sendToServer('ready', {name: this.window_name});
+}
+
+WindowManagerClient.prototype.open = function(window_name, args) {
+  sendToServer('open', {name: window_name, args: args});
+}
+
+WindowManagerClient.prototype.close = function(window_name = this.window_name) {
+  sendToServer('close', {target: window_name});
+}
+
+WindowManagerClient.prototype.send = function(window_name, payload) {
+  sendToServer('send', {target: window_name, payload: payload});
+}
+
+module.exports = WindowManagerClient;
---
+++ src/window_manager_server.js
@@ -0,0 +1,88 @@
+'use strict';
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+var ipc = require('electron').ipcMain;
+
+var instances = [];
+var msg_queue = [];
+
+function WindowManagerServer(windowDefinition) {
+  // constructor: set variable
+  this.window_definition = windowDefinition;
+}
+
+function sendMessage(window_name) {
+  if(!instances.hasOwnProperty(window_name) || typeof instances[window_name] !== 'object') {
+    return;
+  }
+  if(!msg_queue.hasOwnProperty(window_name) || typeof msg_queue[window_name] !== 'object') {
+    return;
+  }
+  while(msg_queue[window_name].length > 0) {
+    var obj = msg_queue[window_name].shift();
+    instances[window_name].webContents.send('WindowManager', obj);
+  }
+}
+
+WindowManagerServer.prototype.open = function(window_name) {
+  if(instances.hasOwnProperty(window_name) && instances[window_name] != null) {
+    return;
+  }
+  var wdef = this.window_definition[window_name];
+  var instance = wdef.create();
+  instance.on('closed', function () {
+      instances[window_name] = null;
+  });
+  instances[window_name] = instance;
+};
+
+WindowManagerServer.prototype.close = function(window_name) {
+  if(this.isOpened(window_name)) {
+    instances[window_name].close();
+  }
+  return;
+};
+
+WindowManagerServer.prototype.isOpened = function(window_name) {
+  if(instances.hasOwnProperty(window_name) && instances[window_name] != null) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+WindowManagerServer.prototype.handleChannelWindowManager = function(event, command) {
+  if(command.action === 'open') {
+    var window_name = command.data.name;
+    var window_args = command.data.args;
+    if(window_args !== undefined && window_args !== null) {
+      if(!msg_queue.hasOwnProperty(window_name) || typeof msg_queue[window_name] !== 'array') {
+        msg_queue[window_name] = [];
+      }
+      msg_queue[window_name].push(window_args);
+    }
+    if(this.isOpened(window_name)) {
+      sendMessage(window_name);
+    } else {
+      this.open(window_name);
+    }
+  } else if (command.action === 'ready') {
+    var window_name = command.data.name;
+    sendMessage(window_name);
+  } else if (command.action === 'send') {
+    var window_name = command.data.target;
+    var payload = command.data.payload;
+    msg_queue[window_name].push(payload);
+    sendMessage(window_name);
+  } else if (command.action === 'close') {
+    var target_window = command.data.target;
+    this.close(target_window);
+  }
+};
+
+WindowManagerServer.prototype.registerIPC = function() {
+  ipc.on('WindowManager', function(event, arg) {
+    this.handleChannelWindowManager(event, arg);
+  }.bind(this));
+}
+
+module.exports = WindowManagerServer;
